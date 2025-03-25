2161b72e034074d055a74de85dd52483ab643633
Rimsha_Shahzadi <rimshashahzadi806@gmail.com>
2025-03-26 03:59:51 +0500

    .

--- langgraph/hello.py
+++ langgraph/hello.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
+    model="gemini-2.0-flash", google_api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def deposit_money(name:str , bank_account:str, amount:int) -> dict:
